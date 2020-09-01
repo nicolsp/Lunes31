a2ccab700caa5f0ff3d102c8c788cd756e6346f8
copito.123 <nicolspnicolsp@gmail.com>
2020-08-31 22:52:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lunes31/Ejercicio1.kt
@@ -0,0 +1,18 @@
+package com.example.lunes31
+
+fun main() {
+  //Ejercicio 1
+    // val name = "nicols suazo perez "
+    // println("Mi nombre es ${name}")
+
+   //Ejercicio 2
+  //  val num = 10
+   // val num2 = 20
+    //val num3 = 30
+    //println("La suma de los tres numeros es ${num+num2+num3}")
+
+    //Ejercicio 3
+
+    val Name: String = "Ark Reigen"
+
+}
---
+++ app/src/main/java/com/example/lunes31/Lunes31.kt
@@ -0,0 +1,106 @@
+package com.example.lunes31
+// tipo de clase que te sirve para depresentar datos ( dataclass)
+data class User(val name: String, val id: Int)
+
+//fun main() {
+    //   val mUser = User("Alex" ,  1)
+    //  println(mUser)
+
+    //println(mUser.copy())
+    //println(mUser.copy("Max"))
+    //println(mUser.copy(id =3))
+
+    //val mUser2 = mUser.copy(id=4)
+    //println(mUser)
+
+    //println(mUser2)
+
+//    fun describeString(naybeString: String?): String {
+
+    //      if (naybeString != null && naybeString.length > 0) {
+    //        return "Largo del string es ${naybeString.length}"
+    //      } else {
+    //    return "Empty or null String"
+    // }
+    //}
+
+    //val nullValue : String?
+    //nullValue = null
+
+    //println( describeString((nullValue)))
+
+    //cases("")
+   // cases(1)
+    //cases(0L)
+    //cases(MyClass())
+  //  cases(Bird(2))
+
+   // cases("Hello")
+//}
+//fun cases(obj: Any) {
+      //  when (obj) {
+        //    1 -> println("one")
+          //  "Hello" -> println("saludo")
+           // is Long -> println("Not a string")
+            //is Bird -> println("Este es un objeto Bird ${Objetito}")
+           // !is String -> println("Not a String")
+            //else -> println("Desconocido")
+       // }
+  //  fun main() {
+     //  val cakes = listOf("carrot" , "chesse", "cholate")
+       //for( element in cakes) {
+         //  println("El pastel es de ${element}")
+/*       var cakesEaten = 0
+       var cakesBaken = 0
+       while (cakesEaten < 5)
+           eatAcake()
+            cakesEaten ++
+       }
+do {
+    bakeAcake()
+    cakesBaken ++
+   } while (cakesBaked < cakesEaten)
+fun eatAcake() = println("comer pastel ")
+fun bakeAcake() = println("prepatr pastel")
+*/
+    //aca termina la funcion main
+
+//class MyClass //clase de ejemplo
+
+//for (c in 'a'..'z'){
+//println(c)
+//}
+  //     for (c in 'z' downTo  'a' step 2)
+    //       println(c)
+  // }
+
+//var num =3
+  //     if(num in 1..4) {
+    //   println("es verdadero")
+
+      // }
+       //if(num !in 6..10){
+         //  println("noesta en el rsango")
+      // }
+
+    /*   val authors = setOf("Shakespeare", "Heniway", "twain")
+       val writers = setOf("Twain", "Shakespeare", "Heniwey" )
+       println(authors == writers)
+       println(authors === writers)
+
+       val name = "criss"
+       val name2 = "alex"
+       println(name == name2)
+       println(name === name2)
+*/
+     /*  class CualquierCosa() {
+        val num: Int
+        init {
+            num = 10
+        }
+        constructor(num2 = Int) {
+            println()
+        }
+    }
+   }
+   */
